0e30543e96c9a110851742c1401a351b9a89d4db
manzoorahmed <kalatymanzoor@gmail.com>
2024-05-04 19:42:26 +0500

    first commit

---
+++ main.ts
@@ -0,0 +1,39 @@
+import { log } from "console";
+import inquirer from "inquirer"
+
+const Currency: any = {
+    USD:1,
+    EUR: 0.91,
+    GPB: 0.76,
+    INR:74.57,
+    PKR: 280,
+    
+};
+let user_answer = await inquirer . prompt(
+    [
+        {
+             name: "from",
+             message:"Enter From Currency ",
+             type:"list",
+             choices:["USD","EUR","GBP","INR","PKR",]
+},
+        {
+              name: "to",
+              message:"Enter To Currency ",
+              type:"list",
+              choices:["USD","EUR","GBP","INR","PKR",]
+}, 
+        {
+              name: "amount",
+              message:"Enter Your Amount ",
+              type:  "number"
+},
+
+]
+)
+let fromAmount = Currency[user_answer.from]
+let toAmount = Currency[user_answer.to]
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount //USD BASE CURRENCY //4
+let convertedAmount = baseAmount * toAmount
+console.log(convertedAmount);
